1f0e37c576491b05529b1ecc012c06a42ce12387
Kassidy Tavel <tavelkassidy@gmail.com>
2022-10-27 15:20:12 -0500

    added files

---
+++ week 13 React/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import'./Login.css';
+import {Redirect} from 'react-router-dom';
+
+class Login extends Component {
+    constructor(props) {
+        super(props); {
+            this.state = {
+                islogged.false,
+                loginParams: {
+                    user_id:''
+                }
+            }
+        }
+    }
+}
